138204f2691c6477a76c9133f7a8f0874d8ac17d
Lesthy <103683378+Lesthy@users.noreply.github.com>
2022-06-06 22:48:58 +0300

    Readme file resources update

--- src/alfa/ProgrammingLanguages.java
+++ src/alfa/ProgrammingLanguages.java
@@ -3,13 +3,13 @@ package alfa;
 public class ProgrammingLanguages {
         // How many characters does it use to print out "Hello World"?
         int simplicity;
-        // What programming language has the highest TIOBE index currently? (Which is the most used language?)
+        // Which programming language is the most popular in 2022, according to TIOBE index?
         int usage;
         //Which is the fastest programming language?
         int fastest;
-        //Which of programming languages are the easiest to learn ?
+        //Which of programming languages is the easiest to learn ?
         int easiest;
-        // Which is the safest?
+        // Which programming language is the most secure?
         int safest;
 }
 
