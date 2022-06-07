094d1d0a929771296870e37314566164ca10d941
rozolins9 <r.ozolins9@gmail.com>
2022-06-07 21:00:00 +0300

    Count score

--- src/alfa/Main.java
+++ src/alfa/Main.java
@@ -15,6 +15,8 @@ public class Main {
         System.out.println("Language Battle");
         System.out.println(" ");
         Boolean infinite = true;
+        int wins = 0;
+        int losses = 0;
         do {
             System.out.println("Choose your language : Java, C, PHP, Python or JavaScript");
             String choice = scan.nextLine();
@@ -45,37 +47,47 @@ public class Main {
             if (arg.equalsIgnoreCase("Syntax")) {
                 if (hero.getSyntax() > enemy.getSyntax()) {
                     System.out.println("You won, congratulations!");
+                    wins ++;
                 } else {
                     System.out.println("Sorry, You lost!");
+                    losses++;
                 }
             } else if (arg.equalsIgnoreCase("Usage")) {
                 if (hero.getUsage() > enemy.getUsage()) {
                     System.out.println("You won, congratulations!");
+                    wins ++;
                 } else {
                     System.out.println("Sorry, You lost!");
+                    losses++;
                 }
             } else if (arg.equalsIgnoreCase("Speed")) {
                 if (hero.getSpeed() > enemy.getSpeed()) {
                     System.out.println("You won, congratulations!");
+                    wins ++;
                 } else {
                     System.out.println("Sorry, You lost!");
+                    losses++;
                 }
             } else if (arg.equalsIgnoreCase("Simplicity")) {
                 if (hero.getSimplicity() > enemy.getSimplicity()) {
                     System.out.println("You won, congratulations!");
+                    wins ++;
                 } else {
                     System.out.println("Sorry, You lost!");
+                    losses++;
                 }
             } else if (arg.equalsIgnoreCase("Security")) {
                 if (hero.getSecurity() > enemy.getSecurity()) {
                     System.out.println("You won, congratulations!");
+                    wins ++;
                 } else {
                     System.out.println("Sorry, You lost!");
+                    losses++;
                 }
             } else {
                 throw new Exception("There is no such option!");
             }
-
+            System.out.println("Wins: " + wins + ", Losses: " + losses);
         } while (infinite);
     }
 
